fc890b499c8790d5e989e946baa0c39ed7eda233
tverdokhlib <tverdokhlibroman1998@gmail.com>
2017-05-23 21:48:02 +0200

    Updated

---
+++ src/assets/js/app.js
@@ -0,0 +1,19 @@
+$(document).foundation();
+
+$(document).ready(function () {
+
+    setupCookieBar();
+    
+    /*$('header ul').on('click', 'a', function (e) {
+        e.preventDefault();
+    });
+    
+    $('header ul').on('click', 'li', function (e) {
+        $('header img').addClass('spinner');
+        var th = $(this);
+        th.parent().find('li').removeClass('active');
+        th.addClass('active');
+        window.location = th.find('a').attr('href');
+    });*/
+    
+});
