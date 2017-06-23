263bc5d416ea1a82907bcf7a67f1827439039d1c
Vlad Trishch <vlad.trishch@gmail.com>
2017-06-23 23:57:05 +0300

    Fixed map, added code to serve contact form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,7 @@
+var hostEmail = 'vlad.trishch@gmail.com'
+/**********************************/
+
+
 $(document).foundation();
 
 $(document).ready(function () {
@@ -17,3 +21,34 @@ $(document).ready(function () {
     });*/
     
 });
+
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+    var firstName = document.getElementById('contact-first-name').value
+    var lastName = document.getElementById('contact-last-name').value
+    var email = document.getElementById('contact-email').value
+    var phone = document.getElementById('contact-phone').value
+    var message = document.getElementById('contact-message').value
+
+    if(!firstName || !lastName || !email || !phone || !message) {
+        alert('Please check the form.')
+    } else {
+        $(this).get(0).reset()
+        $.ajax({
+            url: 'https://formspree.io/'+hostEmail,
+            method: 'POST',
+            data: {
+              "First Name": firstName,
+              "Last Name": lastName,
+              "Email": email,
+              "Phone Number": phone,
+              "Message": message
+            },
+            dataType: 'json'
+        })
+        e.preventDefault()
+        alert('Your message sent.')
+    }
+  })
+})
