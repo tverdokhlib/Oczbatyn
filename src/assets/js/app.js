92c1a77dc710b9c90d6c7737e382293c5998eee0
Vlad Trishch <vlad.trishch@gmail.com>
2017-06-30 01:01:49 +0300

    Fixed mobile version at /methods, /publications/ and /about

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,22 +4,22 @@ var hostEmail = 'kontakt@medycyna-mongolska.pl'
 
 $(document).foundation();
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    setupCookieBar();
+  setupCookieBar();
+
+  /*$('header ul').on('click', 'a', function (e) {
+      e.preventDefault();
+  });
     
-    /*$('header ul').on('click', 'a', function (e) {
-        e.preventDefault();
-    });
-    
-    $('header ul').on('click', 'li', function (e) {
-        $('header img').addClass('spinner');
-        var th = $(this);
-        th.parent().find('li').removeClass('active');
-        th.addClass('active');
-        window.location = th.find('a').attr('href');
-    });*/
-    
+  $('header ul').on('click', 'li', function (e) {
+      $('header img').addClass('spinner');
+      var th = $(this);
+      th.parent().find('li').removeClass('active');
+      th.addClass('active');
+      window.location = th.find('a').attr('href');
+  });*/
+
 });
 
 
@@ -31,24 +31,24 @@ $(document).ready(function() {
     var phone = document.getElementById('contact-phone').value
     var message = document.getElementById('contact-message').value
 
-    if(!firstName || !lastName || !email || !phone || !message) {
-        alert('Please check the form.')
+    if (!firstName || !lastName || !email || !phone || !message) {
+      alert('Please check the form.')
     } else {
-        $(this).get(0).reset()
-        $.ajax({
-            url: 'https://formspree.io/'+hostEmail,
-            method: 'POST',
-            data: {
-              "First Name": firstName,
-              "Last Name": lastName,
-              "Email": email,
-              "Phone Number": phone,
-              "Message": message
-            },
-            dataType: 'json'
-        })
-        e.preventDefault()
-        alert('Your message sent.')
+      $(this).get(0).reset()
+      $.ajax({
+        url: 'https://formspree.io/' + hostEmail,
+        method: 'POST',
+        data: {
+          "First Name": firstName,
+          "Last Name": lastName,
+          "Email": email,
+          "Phone Number": phone,
+          "Message": message
+        },
+        dataType: 'json'
+      })
+      e.preventDefault()
+      alert('Your message sent.')
     }
   })
 })
